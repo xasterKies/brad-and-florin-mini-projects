4a518808167a80f19aaef872ae23da36af9be3e1
TiokengSamuel <samueledward40@gmail.com>
2020-12-05 16:28:09 +0100

    complete project 2

--- PJ2: Prgress Steps/script.js
+++ PJ2: Prgress Steps/script.js
@@ -1,7 +1,7 @@
 const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-const circles = document.getElementById('.circle');
+const circles = document.querySelectorAll('.circle');
 
 let currentActive = 0;
 let newWidth = 0;
@@ -10,9 +10,9 @@ next.addEventListener('click', () => {
     currentActive++;
     newWidth  += 33;
     progress.style.width = newWidth + "%";
-    circles[currentActive].classList.add('active')
+    circles[currentActive].classList.add('active');
     prev.removeAttribute('disabled');
-    if(currentActive = 3) {
+    if(currentActive == 3) {
         next.setAttribute('disabled', '');
     }
 })
