55cdafc96e7441c8a84dc20dabaeb4acc016849a
TiokengSamuel <samueledward40@gmail.com>
2020-12-05 14:55:28 +0100

    progress

--- PJ2: Prgress Steps/script.js
+++ PJ2: Prgress Steps/script.js
@@ -1,1 +1,26 @@
-const 
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.getElementById('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    bar = progress.style.width;
+
+    switch (bar) {
+        case currentActive = 2 :
+            bar = '32%';
+            break;
+        case currentActive = 3 :
+            bar = '64%';
+            break;
+        case currentActive = 4:
+            bar = '96%'
+            break;
+        default:
+            bar = '0%'
+    }
+}
