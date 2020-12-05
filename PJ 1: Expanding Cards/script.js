301fd700e7c735729f615209e0d0b53c47a06e53
TiokengSamuel <samueledward40@gmail.com>
2020-12-05 05:51:37 +0100

    h3 opacity

--- PJ 1: Expanding Cards/script.js
+++ PJ 1: Expanding Cards/script.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
-    panels.addEventListener('click', () => {
+    panel.addEventListener('click', () => {
         removeActiveClasses();
         panel.classList.add('active')
     })
