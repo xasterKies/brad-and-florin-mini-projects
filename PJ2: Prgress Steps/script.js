f311bed20fc62c9684edb5a0ab98987f0fbe3ca4
TiokengSamuel <samueledward40@gmail.com>
2020-12-05 16:19:24 +0100

    prev add

--- PJ2: Prgress Steps/script.js
+++ PJ2: Prgress Steps/script.js
@@ -3,14 +3,24 @@ const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const circles = document.getElementById('.circle');
 
-let currentActive = 1;
+let currentActive = 0;
+let newWidth = 0;
 
 next.addEventListener('click', () => {
-    currentActive++
+    currentActive++;
     newWidth  += 33;
     progress.style.width = newWidth + "%";
     circles[currentActive].classList.add('active')
     prev.removeAttribute('disabled', '');
 })
 
-prev.addEventListener
+prev.addEventListener('click', () => {
+    newWidth -= 33;
+    progress.style.width = newWidth + "%";
+    circles[currentActive].classList.remove('active')
+    currentActive--;
+    if (currentActive == 0) {
+        prev.setAttribute('disabled', '');
+    }
+    next.removeAttribute('disabled');
+})
