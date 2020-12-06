7991c4f02e6b38ba0d8d8fc55859e55789e60af6
TiokengSamuel <samueledward40@gmail.com>
2020-12-06 15:16:14 +0100

    script

--- PJ6: Scroll to Animate/script.js
+++ PJ6: Scroll to Animate/script.js
@@ -8,6 +8,12 @@ function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4;
 
     boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
     })
 }
