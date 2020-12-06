77a73c47424eafb4ac870b2c94387242767d4733
TiokengSamuel <samueledward40@gmail.com>
2020-12-06 17:20:50 +0100

    container

--- PJ7: Split Landing Page/script.js
+++ PJ7: Split Landing Page/script.js
@@ -1,3 +1,5 @@
 const left = document.querySelctor('.left');
 const right = document.querySelector('.right')
 const contianer = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => contianer.classList.add('hover-left'))
