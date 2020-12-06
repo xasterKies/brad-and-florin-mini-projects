bd2335a5220c1239653471443eee95215ae6fabf
TiokengSamuel <samueledward40@gmail.com>
2020-12-06 13:27:37 +0100

    script

--- PJ5: Background Image and Loading Text/script.js
+++ PJ5: Background Image and Loading Text/script.js
@@ -14,7 +14,7 @@ function blurring() {
 
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 
     
 }
