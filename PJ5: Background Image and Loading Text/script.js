86c823c6cdf3e85f8636957a4041a7a2897722a7
TiokengSamuel <samueledward40@gmail.com>
2020-12-06 12:59:43 +0100

    fxn

--- PJ5: Background Image and Loading Text/script.js
+++ PJ5: Background Image and Loading Text/script.js
@@ -15,7 +15,9 @@ function blurring() {
     loadText.innerText = `${load}%`
     loadText.style.opacity = 
 
-    const scale = (num, in_min, in_max out_min, out_max) => {
-        return(num - in_min) * (out_max - out_min) / (in_max - in_min)
-    }
+    
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return(num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
